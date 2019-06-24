adc0c28684b610e2d27498416cd1bdb7c26a527e
Simon Convent <simon.convent@rwth-aachen.de>
2019-06-24 23:16:58 +0200

    Add timing stats to chameleon version

--- src/examples/swe_chameleon.cpp
+++ src/examples/swe_chameleon.cpp
@@ -555,7 +555,20 @@ int main(int argc, char** argv) {
 	__itt_detach();	
 #endif
 
-
+	// temporary stats
+	float minTime=1000000.; float maxTime=0.;float avgTime=0.; int blockCount = 0;
+	for(int x = xBounds[myXRank]; x < xBounds[myXRank+1]; x++) {
+		for(int y = yBounds[myYRank]; y < yBounds[myYRank+1]; y++) {
+			if(blocks[xBounds[myXRank]][myYRank]->computeTimeWall < minTime)
+				minTime = blocks[xBounds[myXRank]][myYRank]->computeTimeWall;
+			if(blocks[xBounds[myXRank]][myYRank]->computeTimeWall > maxTime)
+				maxTime = blocks[xBounds[myXRank]][myYRank]->computeTimeWall;
+			avgTime += blocks[xBounds[myXRank]][myYRank]->computeTimeWall;
+			blockCount++;
+		}
+	}
+	avgTime /= blockCount;
+	printf("%d: min:%F, max:%f, avg:%f\n", myRank, minTime, maxTime, avgTime); 
 	/************
 	 * FINALIZE *
 	 ************/
